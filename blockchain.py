43a336518d9bbdd47b5d1b8af0c6770fc9371d0f
ari <saranari714@gmail.com>
2023-06-26 15:48:31 +0800

    break & continue

--- blockchain.py
+++ blockchain.py
@@ -21,24 +21,44 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
     # Get the user input, transform it from a string to a float and store it in user_input
     user_input = float(input('Your transaction amount please: '))
     return user_input
 
 
-# Get the first transaction input and add the value to the blockchain
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
 
-while True:
-    tx_amount = get_user_input()
-    add_value(tx_amount, get_last_blockchain_value())
 
+def print_blockchain_elements():
     # Output the blockchain list to the console
     for block in blockchain:
         print('Outputting Block')
         print(block)
+
+# Get the first transaction input and add the value to the blockchain
+tx_amount = get_transaction_value()
+add_value(tx_amount)
+
+while True:
+    print('Please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('q: Quit')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'q':
+        break
+    else:
+        print('Input was invalid, please pick a value from the list!')
+    print('Choice registered!')
+
 
 print('Done!')
